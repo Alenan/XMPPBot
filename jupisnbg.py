821232029a28cf92bf929bd098049b1e59aa2f58
Thomas L <tom@dl6tom.de>
2015-09-07 00:46:06 +0200

    join room and print messages

--- jupisnbg.py
+++ jupisnbg.py
@@ -1,18 +1,29 @@
 from getpass import getpass
-from xmpp import Client
+from xmpp import Client, NS_MUC, Presence
 
-user = 'moscito90'
+user = 'moscito'
 server = 'jabber.ccc.de'
+nick = 'bot'
 password = getpass('Password for %s@%s:' % (user, server))
+room = 'jupisnbg@conference.jabber.ccc.de'
+room_password = etpass('Password for %s:' % (room))
 
-def message():
-    pass
+def message_callback(client, stanza):
+    sender = stanza.getFrom()
+    message = stanza.getBody()
+    jid = '%s%s' % (sender.getNode(), sender.getDomain())
+    if jid == room:
+        print('%s: %s' % (sender, message))
 
-client = Client(server)
+client = Client(server, debug=[])
 client.connect()
 while not client.auth(user, password):
     print('Unable to authorize.')
     password = getpass('Password for %s@%s:' % (user, server))
-client.RegisterHandler('message', message)
+client.RegisterHandler('message', message_callback)
 client.sendInitPresence()
-client.Process(1)
+presence = Presence(to='%s/%s' % (room, nick))
+presence.setTag('x', namespace=NS_MUC).setTagData('password', room_password)
+client.send(presence)
+while True:
+    client.Process(1)
